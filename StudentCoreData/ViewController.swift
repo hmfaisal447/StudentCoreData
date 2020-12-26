692efb3f456ae6105c96ea8017792383038ec346
Faisal Saleem <hmfaisal447@gmail.com>
2020-12-25 22:41:35 -0800

    Initial Commit

---
+++ StudentCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudentCoreData
+//
+//  Created by codegradients on 25/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
